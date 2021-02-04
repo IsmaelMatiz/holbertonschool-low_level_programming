4f7602cf4392fd845d6bfb42eed557437dd3c98e
Ismael <ismaelmatiz@gmail.com>
2021-02-03 22:07:34 -0500

    fixing 4, 3 time

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char strin[50];
 
-	strcpy(strin, "\"Programming is like building a multilingual");
+	strin = "\"Programming is like building a multilingual";
 	puts(strin);
 	return (0);
 }
