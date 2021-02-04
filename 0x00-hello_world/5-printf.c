e8f99181bb6facc0ea80d181b6a61b713dcce508
Ismael <ismaelmatiz@gmail.com>
2021-02-04 07:58:21 -0500

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print text in console
+ *
+ * Description: with this funcion we can print text
+ * in the chosole
+ * Return: Text in the console
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
